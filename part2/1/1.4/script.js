147a0b5ec872e67bd34baa1fd1321d359153058d
AnnaKurashko <kuzann88@mail.ru>
2023-03-15 15:06:45 +0700

    add part 2/1

---
+++ part2/1/1.4/script.js
@@ -0,0 +1,6 @@
+let table = document.getElementById('age-table');
+let label = table.querySelectorAll('lable');
+let firstTD = table.querySelector('td');
+let formSearch = document.querySelector('form[name="search"]');
+let firstInput = formSearch.querySelector('input');
+let lastInput = document.querySelector('form[name="search"]>input:last-child');
